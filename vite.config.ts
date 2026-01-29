fea91bfa345b9d2ec7ae2ecc6ba6af69bee0dd7d
Olechurie <aan158426@stud.gibb.ch>
2026-01-29 12:53:40 +0100

    feat: add build configuration for manual chunking of vendor dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,20 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            "react",
+            "react-dom",
+            "framer-motion",
+            "react-router-dom",
+            "axios",
+            "react-select",
+          ],
+        },
+      },
+    },
+  },
 });
